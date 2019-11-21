219469f9e630ce083daf06af0bd4230f51880498
NV4RE <tosbodesz@gmail.com>
2019-11-21 21:27:53 +0700

    Add timeline chart

--- src/containers/TransactionDetail/index.tsx
+++ src/containers/TransactionDetail/index.tsx
@@ -1,8 +1,9 @@
-import { Event } from "@melonade/melonade-declaration";
+import { Event, State } from "@melonade/melonade-declaration";
 import { Icon, Table, Typography } from "antd";
 import moment from "moment";
 import * as R from "ramda";
 import React from "react";
+import { Chart } from "react-google-charts";
 import { RouteComponentProps } from "react-router";
 import styled from "styled-components";
 import JsonViewModal from "../../components/JsonViewModal";
@@ -49,6 +50,108 @@ const getName = (event: Event.AllEvent) => {
     default:
       return undefined;
   }
+};
+
+const getTimelineDataFromEvents = (
+  events: Event.AllEvent[]
+): [string, string, Date, Date][] => {
+  const validEvents = R.reverse(
+    events.filter((event: Event.AllEvent) => !event.isError)
+  );
+
+  const startNodeOfEachType = validEvents.filter((event: Event.AllEvent) => {
+    if (event.isError === true) return false;
+
+    if (event.type === "TASK") {
+      if (event.details.status === State.TaskStates.Scheduled) return true;
+      return false;
+    }
+
+    if (event.type === "WORKFLOW") {
+      if (event.details.status === State.WorkflowStates.Running) return true;
+      return false;
+    }
+
+    if (event.type === "TRANSACTION") {
+      if (event.details.status === State.TransactionStates.Running) return true;
+      return false;
+    }
+  });
+
+  return startNodeOfEachType.map((event: Event.AllEvent) => {
+    if (event.type === "TASK") {
+      const lastTaskEvent = R.findLast(
+        (taskEvent: Event.ITaskEvent) =>
+          event.details.taskId === taskEvent.details.taskId &&
+          taskEvent.details.status !== State.TaskStates.Scheduled,
+        validEvents as Event.ITaskEvent[]
+      );
+
+      let endTimestamp: number = lastTaskEvent
+        ? lastTaskEvent.timestamp
+        : Date.now();
+
+      if (endTimestamp < event.timestamp) endTimestamp = event.timestamp + 1;
+
+      return [
+        "Task",
+        R.pathOr("-", ["details", "taskName"], event),
+        new Date(event.timestamp),
+        new Date(endTimestamp)
+      ];
+    }
+
+    if (event.type === "WORKFLOW") {
+      const lastWorkflowEvent = R.findLast(
+        (workflowEvent: Event.IWorkflowEvent) =>
+          event.details.workflowId === workflowEvent.details.workflowId &&
+          workflowEvent.details.status !== State.WorkflowStates.Running,
+        validEvents as Event.IWorkflowEvent[]
+      );
+
+      let endTimestamp: number = lastWorkflowEvent
+        ? lastWorkflowEvent.timestamp
+        : Date.now();
+
+      if (endTimestamp < event.timestamp) endTimestamp = event.timestamp + 1;
+
+      return [
+        "Workflow",
+        `${R.pathOr(
+          "-",
+          ["details", "workflowDefinition", "name"],
+          event
+        )} / ${R.pathOr("-", ["details", "workflowDefinition", "rev"], event)}`,
+        new Date(event.timestamp),
+        new Date(endTimestamp)
+      ];
+    }
+
+    if (event.type === "TRANSACTION") {
+      const lastTransactionEvent = R.findLast(
+        (transactionEvent: Event.ITransactionEvent) =>
+          event.details.transactionId ===
+            transactionEvent.details.transactionId &&
+          transactionEvent.details.status !== State.TransactionStates.Running,
+        validEvents as Event.ITransactionEvent[]
+      );
+
+      let endTimestamp: number = lastTransactionEvent
+        ? lastTransactionEvent.timestamp
+        : Date.now();
+
+      if (endTimestamp < event.timestamp) endTimestamp = event.timestamp + 1;
+
+      return [
+        "Transaction",
+        event.details.transactionId,
+        new Date(event.timestamp),
+        new Date(endTimestamp)
+      ];
+    }
+
+    return [event.type, "Error", new Date(event.timestamp), new Date()];
+  });
 };
 
 const DateFormat = moment()
@@ -141,10 +244,24 @@ class TransactionTable extends React.Component<IProps, IState> {
 
   render() {
     const { events, isLoading, selectedEventIndex } = this.state;
-
     const isEventSelecting = isNumber(selectedEventIndex);
     return (
       <div>
+        <Chart
+          width={"100%"}
+          height={"300px"}
+          chartType="Timeline"
+          loader={<div>Loading Chart</div>}
+          data={[
+            [
+              { type: "string", id: "Type" },
+              { type: "string", id: "Name" },
+              { type: "date", id: "Start" },
+              { type: "date", id: "End" }
+            ],
+            ...getTimelineDataFromEvents(events)
+          ]}
+        />
         <JsonViewModal
           // @ts-ignore: Fuck the TS lint
           data={isEventSelecting ? events[selectedEventIndex] : []}
