149f86c16b85bf58dec5d542c8e7f2c42ceb33da
NV4RE <tosbodesz@gmail.com>
2019-11-26 11:01:04 +0700

    Able to view workflow

--- src/components/WorkflowChart/tasks.tsx
+++ src/components/WorkflowChart/tasks.tsx
@@ -1,5 +1,5 @@
 import { Task, WorkflowDefinition } from "@melonade/melonade-declaration";
-import { Typography } from "antd";
+import { Icon, Typography } from "antd";
 import * as R from "ramda";
 import React from "react";
 import styled from "styled-components";
@@ -15,6 +15,7 @@ const StartModel = styled.div`
   position: relative;
   height: 35px;
   width: 35px;
+  margin: 6px;
   background-color: black;
   border-radius: 50%;
   display: inline-block;
@@ -37,28 +38,38 @@ const EndModel = styled(StartModel)`
 const TasksContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
-  padding: 12px;
+
+  :hover {
+    background-color: rgba(0, 0, 255, 0.2);
+  }
 `;
 
 const TaskModelContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
-  background-color: pink;
   padding: 12px;
   margin: 6px;
   align-items: center;
   align-self: center;
+  background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
+  border-radius: 4px;
+`;
+
+const SystemTaskModelContainer = styled(TaskModelContainer)`
+  background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
+  border-radius: 14px;
 `;
 
 const ParallelModelContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
-  padding: 12px;
   margin: 6px;
   align-items: center;
-  border-top: 4px solid black;
-  border-bottom: 4px solid black;
   align-self: center;
+
+  :hover {
+    background-color: rgba(0, 0, 255, 0.2);
+  }
 `;
 
 const ParallelModelChildContainer = styled.div`
@@ -66,15 +77,21 @@ const ParallelModelChildContainer = styled.div`
   flex-flow: row nowrap;
   margin: 6px;
   align-items: flex-start;
+
+  border-top: 8px solid black;
+  border-bottom: 8px solid black;
 `;
 
 const DecisionModelContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
-  padding: 12px;
   margin: 6px;
   align-items: center;
   align-self: center;
+
+  :hover {
+    background-color: rgba(0, 0, 255, 0.2);
+  }
 `;
 
 const DecisionModelChildContainer = styled.div`
@@ -82,6 +99,39 @@ const DecisionModelChildContainer = styled.div`
   flex-flow: row nowrap;
   margin: 6px;
   align-items: flex-start;
+
+  border-top: 8px dashed black;
+  border-bottom: 8px dashed black;
+`;
+
+const DecisionCaseContainer = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  margin: 6px;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+
+  :hover {
+    background-color: rgba(0, 0, 255, 0.2);
+  }
+`;
+
+const DecisionCaseModelContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(to top right, orange 50%, transparent 50%) top
+      right,
+    linear-gradient(to top left, orange 50%, transparent 50%) top left,
+    linear-gradient(to bottom right, orange 50%, transparent 50%) bottom right,
+    linear-gradient(to bottom left, orange 50%, transparent 50%) bottom left;
+  background-size: 50% 50%;
+  background-repeat: no-repeat;
+
+  width: 160px;
+  height: 80px;
+  margin: 6px;
 `;
 
 interface ITaskProps {
@@ -101,7 +151,12 @@ interface IParallelProps {
 
 const ParallelModel = (props: IParallelProps) => (
   <ParallelModelContainer>
-    <Typography.Text>{props.task.taskReferenceName}</Typography.Text>
+    <SystemTaskModelContainer>
+      <Typography.Text>{props.task.taskReferenceName}</Typography.Text>
+      <Typography.Text code>PARALLEL</Typography.Text>
+    </SystemTaskModelContainer>
+
+    <Icon type="arrow-down" />
 
     <ParallelModelChildContainer>
       {props.task.parallelTasks.map(
@@ -113,25 +168,62 @@ const ParallelModel = (props: IParallelProps) => (
   </ParallelModelContainer>
 );
 
+interface IDecisionCaseProps {
+  tasks: WorkflowDefinition.AllTaskType[];
+  caseKey: string;
+}
+
+const DecisionCase = (props: IDecisionCaseProps) => (
+  <DecisionCaseContainer>
+    <DecisionCaseModelContainer>
+      <Typography.Text>{props.caseKey}</Typography.Text>
+    </DecisionCaseModelContainer>
+    <Icon type="arrow-down" />
+    <RenderChildTasks tasks={props.tasks} />
+  </DecisionCaseContainer>
+);
+
 interface IDecisionProps {
   task: WorkflowDefinition.IDecisionTask;
 }
 
 const DecisionModel = (props: IDecisionProps) => (
   <DecisionModelContainer>
-    <Typography.Text>{props.task.taskReferenceName}</Typography.Text>
+    <SystemTaskModelContainer>
+      <Typography.Text>{props.task.taskReferenceName}</Typography.Text>
+      <Typography.Text code>DECISION</Typography.Text>
+    </SystemTaskModelContainer>
+
+    <Icon type="arrow-down" />
 
     <DecisionModelChildContainer>
       {R.toPairs(props.task.decisions).map(
-        ([_case, tasks]: [string, WorkflowDefinition.AllTaskType[]]) => (
-          <RenderChildTasks tasks={tasks} />
+        ([caseKey, tasks]: [string, WorkflowDefinition.AllTaskType[]]) => (
+          <DecisionCase tasks={tasks} caseKey={caseKey} />
         )
       )}
-
-      <RenderChildTasks tasks={props.task.defaultDecision} />
+      <DecisionCase tasks={props.task.defaultDecision} caseKey="default" />
     </DecisionModelChildContainer>
   </DecisionModelContainer>
 );
+
+interface IAllTaskProps {
+  task: WorkflowDefinition.AllTaskType;
+}
+
+const AllTaskModel = (props: IAllTaskProps) => {
+  const { task } = props;
+  switch (task.type) {
+    case Task.TaskTypes.Task:
+      return <TaskModel task={task} />;
+    case Task.TaskTypes.Parallel:
+      return <ParallelModel task={task} />;
+    case Task.TaskTypes.Decision:
+      return <DecisionModel task={task} />;
+    default:
+      return <Icon type="warning" />;
+  }
+};
 
 interface IProps {
   tasks: WorkflowDefinition.AllTaskType[];
@@ -139,24 +231,19 @@ interface IProps {
 
 const RenderChildTasks = (props: IProps) => (
   <TasksContainer>
-    {props.tasks.map((task: WorkflowDefinition.AllTaskType) => {
-      switch (task.type) {
-        case Task.TaskTypes.Task:
-          return <TaskModel task={task} />;
-        case Task.TaskTypes.Parallel:
-          return <ParallelModel task={task} />;
-        case Task.TaskTypes.Decision:
-          return <DecisionModel task={task} />;
-        default:
-          return <div />;
-      }
-    })}
+    {props.tasks.map((task: WorkflowDefinition.AllTaskType) => (
+      <React.Fragment>
+        <AllTaskModel task={task} />
+        <Icon type="arrow-down" />
+      </React.Fragment>
+    ))}
   </TasksContainer>
 );
 
 export default (props: IProps) => (
   <ChartContainer>
     <StartModel />
+    <Icon type="arrow-down" />
     <RenderChildTasks tasks={props.tasks} />
     <EndModel />
   </ChartContainer>
