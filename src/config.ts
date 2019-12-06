fd8aea1f023e62e70adabbe900f6d3f04b86a710
NV4RE <tosbodesz@gmail.com>
2019-12-06 18:55:05 +0700

    update config for nginx

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,15 @@
 export const eventLogger = {
   http: {
-    baseURL: process.env["REACT_APP_EVENT_LOGGER_HTTP_BASEURL"]
+    baseURL:
+      process.env["REACT_APP_EVENT_LOGGER_HTTP_BASEURL"] ||
+      "/api/process-manager/"
   }
 };
 
 export const processManager = {
   http: {
-    baseURL: process.env["REACT_APP_PROCESS_MANAGER_HTTP_BASEURL"]
+    baseURL:
+      process.env["REACT_APP_PROCESS_MANAGER_HTTP_BASEURL"] ||
+      "/api/event-logger/"
   }
 };
