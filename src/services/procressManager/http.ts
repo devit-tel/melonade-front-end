6d051fdae6e5cc71bef3cbbc53fab0e3802e3ac7
NV4RE <tosbodesz@gmail.com>
2020-03-02 16:42:18 +0700

    Able to delete workflow def

--- src/services/procressManager/http.ts
+++ src/services/procressManager/http.ts
@@ -66,13 +66,13 @@ export const createTaskDefinitions = async (
   return R.path(["data", "data"], resp) as TaskDefinition.ITaskDefinition;
 };
 
-export const updateWorkflowDefinitions = async (
-  taskDefinition: WorkflowDefinition.IWorkflowDefinition
+export const updateWorkflowDefinition = async (
+  workflowDefinition: WorkflowDefinition.IWorkflowDefinition
 ): Promise<WorkflowDefinition.IWorkflowDefinition> => {
   const resp = await client({
     url: "/v1/definition/workflow",
     method: "PUT",
-    data: taskDefinition
+    data: workflowDefinition
   });
 
   return R.path(
@@ -81,19 +81,29 @@ export const updateWorkflowDefinitions = async (
   ) as WorkflowDefinition.IWorkflowDefinition;
 };
 
-export const createWorkflowDefinitions = async (
-  taskDefinition: WorkflowDefinition.IWorkflowDefinition
+export const createWorkflowDefinition = async (
+  workflowDefinition: WorkflowDefinition.IWorkflowDefinition
 ): Promise<WorkflowDefinition.IWorkflowDefinition> => {
   const resp = await client({
     url: "/v1/definition/workflow",
     method: "POST",
-    data: taskDefinition
+    data: workflowDefinition
   });
 
   return R.path(
     ["data", "data"],
     resp
   ) as WorkflowDefinition.IWorkflowDefinition;
+};
+
+export const deleteWorkflowDefinition = async (
+  name: string,
+  rev: string
+): Promise<void> => {
+  await client({
+    url: `/v1/definition/workflow/${name}/${rev}`,
+    method: "DELETE"
+  });
 };
 
 export const cancelTranasaction = async (
